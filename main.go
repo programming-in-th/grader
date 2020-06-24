27226e95dcb9cb64be5aa892a81cf0ca93c15aa8
Schwinn Saereesitthipitak <secure@szawinis.com>
2020-06-24 16:24:31 +0700

    Added capability for parallelization across submissions

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"sync"
 
 	"github.com/programming-in-th/grader/api"
 	"github.com/programming-in-th/grader/conf"
@@ -23,30 +24,47 @@ func initGrader(config conf.Config) {
 		log.Fatalln("Error initializing API: cannot create base src path")
 	}
 
-	requestChannel := make(chan api.GradingRequest)
 	gradingJobDoneChannel := make(chan bool)
 	gradingJobChannel := grader.NewGradingJobQueue(2, gradingJobDoneChannel, config)
 
 	// Init handlers
-	go submissionWorker(requestChannel, gradingJobChannel, config)
+	requestDoneChannel := make(chan bool)
+	requestChannel := newSubmissionJobQueue(4, requestDoneChannel, gradingJobChannel, config)
 	api.InitAPI(requestChannel, config)
 
-	close(requestChannel)
+	requestDoneChannel <- true
 	gradingJobDoneChannel <- true
 	close(gradingJobDoneChannel)
 }
 
-func submissionWorker(requestChannel chan api.GradingRequest, gradingJobChannel chan grader.GradingJob, config conf.Config) {
-	for {
-		select {
-		case request := <-requestChannel:
-			err := grader.GradeSubmission(request.SubmissionID, request.TaskID, request.TargLang, request.Code, gradingJobChannel, request.SyncUpdateChannel, config)
-			if err != nil {
-				// TODO: do something with the error
-				log.Println(err)
+func newSubmissionJobQueue(maxWorkers int, done chan bool, gradingJobChannel chan grader.GradingJob, config conf.Config) chan api.GradingRequest {
+	ch := make(chan api.GradingRequest)
+	var wg sync.WaitGroup
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	wg.Add(maxWorkers)
+	for i := 0; i < maxWorkers; i++ {
+		go func() {
+			for {
+				select {
+				case request := <-ch:
+					err := grader.GradeSubmission(request.SubmissionID, request.TaskID, request.TargLang, request.Code, gradingJobChannel, request.SyncUpdateChannel, config)
+					if err != nil {
+						// TODO: do something with the error
+						log.Println(err)
+					}
+				case <-done:
+					wg.Done()
+					return
+				}
 			}
-		}
+		}()
 	}
+	return ch
 }
 
 func main() {
